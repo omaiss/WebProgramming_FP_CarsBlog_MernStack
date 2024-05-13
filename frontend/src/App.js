40d01ea97ddf4f3d0e98c140aa387b2cca689e73
omaisafzal <126196753+omaiss@users.noreply.github.com>
2024-05-14 00:00:02 +0500

    Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
 import Login from './components/account/Login';
-import {Box} from "@mui/material";
-import autoblog from './images/autoblog.png'
   
 function App() {
-  // const img_url = "autoblog.png";
   return (
-    <div className="App">
-    <Box>
-    <img src={autoblog}></img>
+    <div className='App'>
       <Login />
-      </Box>
     </div>
   );
 }
 
 export default App;
-
-//   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
